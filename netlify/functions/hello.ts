ff2eb763dd6faa28d5fe69a727508d80e60a46ea
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-03-27 00:57:51 -0400

    Add conditional hello string

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -5,6 +5,13 @@ import { env } from "~/env";
 export default async function handler(_req: Request, _context: Context) {
   const e = env.NODE_ENV;
   console.log(e);
+  if (e === "development") {
+    return new Response("Hello, development!");
+  } else if (e === "test") {
+    return new Response("Hello, test!");
+  } else if (e === "production") {
+    return new Response("Hello, production!");
+  }
   return new Response("Hello, world!");
 }
 

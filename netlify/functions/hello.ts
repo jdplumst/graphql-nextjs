240152656b09e782e19276e7e2cb2644705ade73
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-03-27 00:53:10 -0400

    Remove db schema

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,7 +1,10 @@
 import type { Config, Context } from "@netlify/functions";
+import { env } from "~/env";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async function handler(_req: Request, _context: Context) {
+  const e = env.NODE_ENV;
+  console.log(e);
   return new Response("Hello, world!");
 }
 
--- src/server/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const posts = sqliteTable("posts", {
-  id: int("id").primaryKey(),
-  title: text("title").notNull(),
-  content: text("content"),
-});
